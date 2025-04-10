0e719bd4b5b0d97427737afb35181b02b9ac70d8
luwnejie <alwjlola@gmail.com>
2025-04-10 17:47:01 +0800

    add StatelessViewModel

--- lib/view_model.dart
+++ lib/view_model.dart
@@ -2,3 +2,4 @@ library view_model;
 
 export "package:view_model/src/view_model/extension.dart";
 export "package:view_model/src/view_model/view_model.dart";
+export "package:view_model/src/view_model/stateless_view_model.dart";
